1cf6abb336ced076ea90d0b6260ebbbdbfbf7bfe
Spiivak <edenspiivak@gmail.com>
2023-12-25 17:42:14 +0200

    Import cmps

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -5,13 +5,14 @@ import './assets/style/main.css'
 // const { Route, Routes } = ReactRouterDOM
 // const { Provider } = ReactRedux
 
-// import { AppHeader } from './cmps/AppHeader'
+import { AppHeader } from './cmps/AppHeader'
 // import { AppFooter } from './cmps/AppFooter'
 
 // import { HomePage } from './pages/HomePage'
 // import { AboutUs } from './pages/AboutUs'
 // import { CarIndex } from './pages/CarIndex.jsx'
 import { store } from './store/store.js'
+import { ToyIndex } from './pages/ToyIndex.jsx'
 // import { CarDetails } from './pages/CarDetails.jsx'
 
 
@@ -20,19 +21,18 @@ export function App() {
     return (
         <Provider store={store}>
             <Router>
-                <h1>Hello World</h1>
-                {/* <section className="main-layout app">
+                <section className="main-layout app">
                     <AppHeader />
                     <main>
                         <Routes>
-                            <Route element={<HomePage />} path="/" />
-                            <Route element={<AboutUs />} path="/about" />
-                            <Route element={<CarDetails />} path="/car/:carId" />
-                            <Route element={<CarIndex />} path="/car" />
+                            {/* <Route element={<HomePage />} path="/" /> */}
+                            {/* <Route element={<AboutUs />} path="/about" /> */}
+                            {/* <Route element={<CarDetails />} path="/car/:carId" /> */}
+                            <Route element={<ToyIndex />} path="/toy" />
                         </Routes>
                     </main>
-                    <AppFooter />
-                </section> */}
+                    {/* <AppFooter /> */}
+                </section>
             </Router>
         </Provider>
     )
