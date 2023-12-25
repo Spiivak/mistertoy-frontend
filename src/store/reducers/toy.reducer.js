20929e8ed308875cce603371aeda9cc8dba04822
Spiivak <edenspiivak@gmail.com>
2023-12-25 13:58:53 +0200

    Impliment store

---
+++ src/store/reducers/toy.reducer.js
@@ -0,0 +1,58 @@
+import { toyService } from "../../services/toy.service.js"
+
+
+// toy
+export const SET_TOYS = 'SET_TOYS'
+export const REMOVE_TOY = 'REMOVE_TOY'
+export const ADD_TOY = 'ADD_TOY'
+export const UPDATE_TOY = 'UPDATE_TOY'
+export const TOY_UNDO = 'TOY_UNDO'
+
+export const SET_IS_LOADING = 'SET_IS_LOADING'
+
+export const SET_FILTER_BY = 'SET_FILTER_BY'
+
+const initialState = {
+    toys: [],
+    lastToys: [],
+    isLoading: false,
+    filterBy: toyService.getDefaultFilter()
+}
+
+export function toyReducer(state = initialState, action = {}) {
+
+    let toys
+    let lastToys
+    switch (action.type) {
+        // toy
+        case SET_TOYS:
+            return { ...state, toys: action.toys }
+
+        case REMOVE_TOY:
+            lastToys = [...state.toys]
+            toys = state.toys.filter(toy => toy._id !== action.toyId)
+            return { ...state, toys, lastToys }
+
+        case ADD_TOY:
+            toys = [...state.toys, action.toy]
+            return { ...state, toys }
+
+        case UPDATE_TOY:
+            toys = state.toys.map(toy => toy._id === action.toy._id ? action.toy : toy)
+            return { ...state, toys }
+
+
+        case SET_IS_LOADING:
+            return { ...state, isLoading: action.isLoading }
+
+        case SET_FILTER_BY:
+            return { ...state, filterBy: { ...state.filterBy, ...action.filterBy } }
+
+        case TOY_UNDO:
+            toys = [...state.lastToys]
+            return { ...state, toys }
+
+        default:
+            return state
+    }
+}
---
+++ src/store/reducers/user.reducer.js
@@ -0,0 +1,20 @@
+import { userService } from "../../services/user.service.js"
+
+/// user
+export const SET_USER = 'SET_USER'
+export const SET_USER_SCORE = 'SET_USER_SCORE'
+
+const initialState = {
+    loggedinUser: userService.getLoggedinUser(),
+}
+
+export function userReducer(state = initialState, action = {}) {
+
+    switch (action.type) {
+        // user
+        case SET_USER:
+            return { ...state, loggedinUser: action.user }
+        default:
+            return state
+    }
+}
