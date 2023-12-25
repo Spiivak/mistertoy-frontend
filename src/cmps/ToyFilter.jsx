56fc61c63be6191fab55216e020033153cbaffa3
Spiivak <edenspiivak@gmail.com>
2023-12-25 19:15:50 +0200

    Add filter

--- src/cmps/ToyFilter.jsx
+++ src/cmps/ToyFilter.jsx
@@ -5,34 +5,55 @@ import { utilService } from "../services/util.service.js"
 
 
 export function ToyFilter({ filterBy, onSetFilter }) {
-    
+
     const [filterByToEdit, setFilterByToEdit] = useState({ ...filterBy })
     onSetFilter = useRef(utilService.debounce(onSetFilter))
 
     useEffect(() => {
         onSetFilter.current(filterByToEdit)
+        console.log('useEffect  filterByToEdit:', filterByToEdit)
     }, [filterByToEdit])
 
+    // function handleChange({ target }) {
+    //     let { value, name: field, type } = target
+    //     value = type === 'number' ? +value : value
+    //     setFilterByToEdit((prevFilter) => ({ ...prevFilter, [field]: value }))
+    // }
+
     function handleChange({ target }) {
-        let { value, name: field, type } = target
-        value = type === 'number' ? +value : value
-        setFilterByToEdit((prevFilter) => ({ ...prevFilter, [field]: value }))
+        const field = target.name;
+        let value = target.value;
+
+        switch (target.type) {
+            case 'number':
+            case 'range':
+                value = +value || '';
+                break;
+
+            case 'checkbox':
+                value = target.checked;
+                break;
+
+            default:
+                break;
+        }
+        setFilterByToEdit((prevFilter) => ({ ...prevFilter, [field]: value }));
     }
+
+
 
     return (
         <section className="toy-filter full main-layout">
             <h2>Toys Filter</h2>
             <form >
-                <label htmlFor="name">Name:</label>
                 <input type="text"
                     id="name"
-                    name="txt"
+                    name="name"
                     placeholder="By Name"
-                    value={filterByToEdit.txt}
+                    value={filterByToEdit.name || ''}
                     onChange={handleChange}
                 />
 
-                <label htmlFor="maxPrice">Max price:</label>
                 <input type="number"
                     id="maxPrice"
                     name="maxPrice"
@@ -40,6 +61,18 @@ export function ToyFilter({ filterBy, onSetFilter }) {
                     value={filterByToEdit.maxPrice || ''}
                     onChange={handleChange}
                 />
+
+                <label htmlFor="inStock">In Stock:</label>
+                <select
+                    id="inStock"
+                    name="inStock"
+                    value={filterByToEdit.inStock || ''}
+                    onChange={handleChange}
+                >
+                    <option value="all">All</option>
+                    <option value={true}>In Stock</option>
+                    <option value={false}>Not In Stock</option>
+                </select>
 
             </form>
 
