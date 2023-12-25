b91b727fdb67bd22acfe873cabb2e20a610ca9b2
Spiivak <edenspiivak@gmail.com>
2023-12-25 17:40:28 +0200

    Update to work with storage service

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -2,13 +2,17 @@
 import Axios from 'axios'
 import { utilService } from './util.service.js'
 import { httpService } from './http.service.js'
+import { storageService } from './async-storage.service.js'
 
+
+const TOY_KEY = 'toysDB'
+// setDemoData()
 // for cookies
-const axios = Axios.create({
-    withCredentials: true
-})
+// const axios = Axios.create({
+//     withCredentials: true
+// })
 
-const BASE_URL = 'toy/'
+// const BASE_URL = '/api/toy'
 
 export const toyService = {
     query,
@@ -19,39 +23,49 @@ export const toyService = {
     getDefaultFilter
 }
 
-function query(filterBy = {}) {
-    return httpService.get(BASE_URL, filterBy)
+function query() {
+  return storageService.query(TOY_KEY)
+
 }
 
 function getById(toyId) {
-    return httpService.get(BASE_URL + toyId)
+    return storageService.get(TOY_KEY, toyId)
 }
 
 function remove(toyId) {
-    return httpService.delete(BASE_URL + toyId)
+    return storageService.remove(TOY_KEY, toyId)
 }
 
 function save(toy) {
     if (toy._id) {
-        return httpService.put(BASE_URL, toy)
+        return storageService.put(TOY_KEY, toy)
     } else {
-        return httpService.post(BASE_URL, toy)
+        return storageService.post(TOY_KEY, toy)
     }
 }
 
 
 function getEmptyToy() {
     return {
-        // vendor: 'Susita-' + (Date.now() % 1000),
-        // price: utilService.getRandomIntInclusive(1000, 9000),
-        // speed: utilService.getRandomIntInclusive(75, 200),
-        name: utilService.makeLorem(2),
+        name: utilService.makeLorem(5),
         price: utilService.getRandomIntInclusive(15, 1500),
         labels: ['label1', 'label2', 'label3'],
         createdAt: new Date(),
-        inStock: Boolean,
+        inStock: true,
     }
 }
+
+// function setDemoData() {
+//   if (!toys && !toys.length) {
+
+//     const toys = [
+//       getEmptyToy(),
+//       getEmptyToy(),
+//       getEmptyToy()
+//     ]
+//     utilService.saveToStorage(TOY_KEY, toys)
+//   }
+// }
 
 
 function getDefaultFilter() {
