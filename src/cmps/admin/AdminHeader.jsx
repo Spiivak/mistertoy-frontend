cafb930040fc4cfdf54448dc189682d540008eed
Spiivak <edenspiivak@gmail.com>
2023-12-27 00:33:13 +0200

    Add Admin Header

---
+++ src/cmps/admin/AdminHeader.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { useSelector } from "react-redux"
+import { NavLink, useLocation } from "react-router-dom"
+
+export function AdminHeader() {
+  const location = useLocation()
+  const user = useSelector(storeState => storeState.userModule.loggedinUser)
+
+  if (!user) return
+  return (
+    <>
+    {location.pathname.includes('/admin') && <header className="admin-header flex full">
+      <nav className="admin-nav flex">
+        <NavLink to="/toy" >Toy</NavLink>
+      </nav>
+    </header>}
+    </>
+  )
+}
