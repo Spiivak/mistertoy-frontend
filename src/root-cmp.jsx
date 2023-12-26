d6007efbef478f3c0462613df6376e032b6f98dd
Spiivak <edenspiivak@gmail.com>
2023-12-27 00:30:22 +0200

    Add more routes to the admin panel

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,41 +1,38 @@
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Routes, BrowserRouter as Router, useLocation } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import './assets/style/main.css'
-// const Router = ReactRouterDOM.BrowserRouter
-// const { Route, Routes } = ReactRouterDOM
-// const { Provider } = ReactRedux
 
 import { AppHeader } from './cmps/AppHeader'
-// import { AppFooter } from './cmps/AppFooter'
-
-// import { HomePage } from './pages/HomePage'
-// import { AboutUs } from './pages/AboutUs'
-// import { CarIndex } from './pages/CarIndex.jsx'
 import { store } from './store/store.js'
 import { ToyIndex } from './pages/ToyIndex.jsx'
 import { ToyDetails } from './pages/ToyDetails.jsx'
-import { AdminHeader } from './cmps/AdminHeader.jsx'
-// import { CarDetails } from './pages/CarDetails.jsx'
+import { AdminHeader } from './cmps/admin/AdminHeader.jsx'
+import {AdminIndex} from './pages/AdminIndex.jsx'
+import {AdminDash} from './pages/AdminDash.jsx'
 
 
 export function App() {
+    // const navigate = useNavigate();
+    // console.log('App  navigate:', navigate)
+    // const location = useLocation()
 
     return (
         <Provider store={store}>
             <Router>
-                <section className="main-layout app">
-                    <AdminHeader/>
+                <>
                     <AppHeader />
-                    <main>
+                    {/* {!location.pathname.includes('/admin') && <AppHeader />} */}
                         <Routes>
                             {/* <Route element={<HomePage />} path="/" /> */}
                             {/* <Route element={<AboutUs />} path="/about" /> */}
                             <Route element={<ToyDetails />} path="/toy/:toyId" />
                             <Route element={<ToyIndex />} path="/toy" />
+                            <Route element={<AdminIndex />} path="/admin">
+                                <Route element={<AdminDash />} path="/admin/dashboard" />
+                            </Route>
                         </Routes>
-                    </main>
                     {/* <AppFooter /> */}
-                </section>
+                </>
             </Router>
         </Provider>
     )
