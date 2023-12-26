e424ea714189406295352d9bbefea409f9ce0ce7
Spiivak <edenspiivak@gmail.com>
2023-12-27 00:31:54 +0200

    Update to work seperatly from the admin header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,53 +1,79 @@
-import MisterToyLogo from '../assets/img/mistertoy-logo.png'
-import { useDispatch, useSelector } from 'react-redux'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { userService } from '../services/user.service'
-import { SET_USER } from '../store/reducers/user.reducer'
-import { LoginSignup } from './LoginSignup'
+// AppHeader.jsx
+import MisterToyLogo from '../assets/img/mistertoy-logo.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { userService } from '../services/user.service';
+import { SET_USER } from '../store/reducers/user.reducer';
+import React, { useState } from 'react';
+import { Modal } from './Modal';
 
 export function AppHeader() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const user = useSelector(storeState => storeState.userModule.loggedinUser)
+  const user = useSelector((storeState) => storeState.userModule.loggedinUser);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoginMode, setIsLoginMode] = useState(true);
 
   function onLogout() {
-    userService.logout()
+    userService
+      .logout()
       .then(() => {
-        onSetUser(null)
+        onSetUser(null);
       })
       .catch((err) => {
-        showErrorMsg('OOPs try again')
-      })
+        showErrorMsg('OOPs try again');
+      });
   }
 
   function onSetUser(user) {
-    dispatch({ type: SET_USER, user })
-    navigate('/')
+    dispatch({ type: SET_USER, user });
+    navigate('/');
   }
 
+  const openModal = (loginMode) => {
+    setIsLoginMode(loginMode);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <header className='app-header flex space-between align-center full'>
-      <div className="logo">
-        <img src={MisterToyLogo} alt="" />
-      </div>
-      <div className="links">
-        <nav className="app-nav flex">
-          <NavLink to="/" >Home</NavLink>
-          <NavLink to="/about" >About</NavLink>
-          <NavLink to="/toy" >Toys</NavLink>
-        </nav>
-      </div>
-      {user ? (
-        < section >
-          <span to={`/user/${user._id}`}>Hello {user.fullname}</span>
-          <button onClick={onLogout}>Logout</button>
-        </ section >
-      ) : (
-        <section>
-          <LoginSignup onSetUser={onSetUser} />
-        </section>
+    <>
+      {!location.pathname.includes('/admin') && (
+        <header className="app-header flex space-between align-center full">
+          <div className="logo">
+            <img src={MisterToyLogo} alt="" />
+          </div>
+          <div className="links">
+            <nav className="app-nav flex">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/toy">Toys</NavLink>
+            </nav>
+          </div>
+          {user ? (
+            <section>
+              <span to={`/user/${user._id}`}>Hello {user.fullname}</span>
+              <button onClick={onLogout}>Logout</button>
+            </section>
+          ) : (
+            <section>
+              <button onClick={() => openModal(true)}>Login</button>
+              <button onClick={() => openModal(false)}>Register</button>
+            </section>
+          )}
+        </header>
       )}
-    </header>
-  )
+
+      {/* Render Modal */}
+      {isModalOpen && (
+        <Modal onClose={closeModal} onSetUser={onSetUser} isLoginMode={isLoginMode} />
+      )}
+    </>
+  );
 }
