60a660bbaa97a02d15b43d1b00259020f0e515f8
Spiivak <edenspiivak@gmail.com>
2023-12-27 00:35:31 +0200

    Add Admin Index

---
+++ src/pages/AdminIndex.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { AdminHeader } from '../cmps/admin/AdminHeader'
+import {AdminAside} from '../cmps/admin/AdminAside'
+
+export function AdminIndex() {
+  const navigate = useNavigate()
+  const user = useSelector(storeState => storeState.userModule.loggedinUser)
+
+  useEffect(() =>{
+    if (!user || !user.isAdmin) navigate('/')
+  }, [])
+
+  if (!user) return
+  return (
+    <section className='admin-index'>
+    <AdminHeader />
+    <AdminAside />
+    <section className="main-admin-content">
+
+    <Outlet />
+    </section>
+    </section>
+  )
+}
