0c34b75d89813c76158db17acfef603ccd8bc59b
Spiivak <edenspiivak@gmail.com>
2023-12-27 00:35:25 +0200

    Admin Dashboard tab

---
+++ src/pages/AdminDash.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { PriceBarChart } from '../cmps/admin/charts/InventoryByLabelChart'
+import { InvByLabel } from '../cmps/admin/charts/InvByLabel'
+import { PricesPerLabel } from '../cmps/admin/charts/PricesPerLabel'
+import { RandomNumbers } from '../cmps/admin/charts/RandomNumbers'
+
+export function AdminDash() {
+  return (
+    <section className='admin-dashboard flex'>
+    <InvByLabel />
+    <PricesPerLabel />
+    <RandomNumbers />
+    {/* <PricesPerLabelChart /> */}
+    </section>
+  )
+}
