548f4d03d515bc252e5281119c8b24bca78e3660
Spiivak <edenspiivak@gmail.com>
2023-12-27 10:58:44 +0200

    Refactor Login Form

--- src/cmps/LoginForm.jsx
+++ src/cmps/LoginForm.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { userService } from '../services/user.service'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const LoginValidationSchema = Yup.object().shape({
   username: Yup.string().required('Required'),
@@ -23,32 +25,33 @@ export function LoginForm({ onLogin }) {
   })
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label htmlFor="username">User Name:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.username}
-        />
-        {formik.touched.username && formik.errors.username && <div>{formik.errors.username}</div>}
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-        />
-        {formik.touched.password && formik.errors.password && <div>{formik.errors.password}</div>}
-      </div>
-      <button type="submit">Login</button>
+    <form onSubmit={formik.handleSubmit} className='login-form flex column'>
+      <TextField
+        id="username"
+        name="username"
+        autoComplete="username"
+        label="User Name"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.username}
+        error={formik.touched.username && Boolean(formik.errors.username)}
+        helperText={formik.touched.username && formik.errors.username}
+      />
+      <TextField
+        type="password"
+        id="password"
+        name="password"
+        label="Password"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helperText={formik.touched.password && formik.errors.password}
+        autoComplete='current-password'
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Login
+      </Button>
     </form>
   )
 }
