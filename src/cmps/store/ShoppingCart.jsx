fe0faf7c595573841896d57e6daac14be603c003
Spiivak <edenspiivak@gmail.com>
2024-01-25 17:04:21 +0200

    Remove comments

--- src/cmps/store/ShoppingCart.jsx
+++ src/cmps/store/ShoppingCart.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { toyService } from '../../services/toy.service'
-// import { getById } from '../../services/toyService'
 
 export function ShoppingCart() {
     const cart = useSelector(storeState => storeState.toyModule.shoppingCart)
@@ -9,7 +8,6 @@ export function ShoppingCart() {
     const [cartItems, setCartItems] = useState([])
 
     useEffect(() => {
-        // Function to fetch details for each toy in the cart
         const fetchCartItemsDetails = async () => {
             try {
                 const itemsDetails = await Promise.all(cart.map(toyId => toyService.getById(toyId)))
@@ -19,7 +17,6 @@ export function ShoppingCart() {
             }
         }
 
-        // Call the fetch function
         fetchCartItemsDetails()
     }, [cart])
 
@@ -29,15 +26,14 @@ export function ShoppingCart() {
             <h1>Shopping Cart</h1>
             <ul className='flex column gap16'>
                 {cartItems.map(item => {
-                {console.log('ShoppingCart  item:', item)}
-                   return <li key={item._id} className='flex align-center gap8'>
-                        
+                    { console.log('ShoppingCart  item:', item) }
+                    return <li key={item._id} className='flex align-center gap8'>
+
                         <img src={item.img} className='item-image' alt="" />
                         <div className="flex column">
-                        <p className='item-name'>{item.name}</p>
-                        <p className='item-price'>Price: ${item.price.toLocaleString()}</p>
+                            <p className='item-name'>{item.name}</p>
+                            <p className='item-price'>Price: ${item.price.toLocaleString()}</p>
                         </div>
-                        {/* Add other details you want to display */}
                     </li>
                 })}
             </ul>
